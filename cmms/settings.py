ae4fc865e8671d5d2bbcb5654342c48f4644fa99
sujay-iEng <sujay@iengaust.com.au>
2026-01-28 12:45:29 +0530

    prod ready1

--- cmms/settings.py
+++ cmms/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cmms_db',
         'USER': 'postgres',
-        'PASSWORD': 'your-db-password-here',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
